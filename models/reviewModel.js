1473c924ae1f028660f6c00083498478538c5bb0
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-07-22 18:43:25 +0200

    Preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Preventing duplicate reviews
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
