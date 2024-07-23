917dfca15c0922619595bb862cb931436ed40b1a
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-07-23 19:14:17 +0200

    Setting up the project structure

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorhandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -91,26 +92,8 @@ app.use((req, res, next) => {
 // .patch(updateUser)
 // .delete(deleteUser);
 
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
 // Version 3.0 for refactoring the code for a better file structure
+app.use('/', viewRouter); // middleware
 app.use('/api/v1/tours', tourRouter); // middleware
 app.use('/api/v1/users', userRouter); // middleware
 app.use('/api/v1/reviews', reviewRouter); // middleware
