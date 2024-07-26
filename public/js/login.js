0dbe4e648080cb99d975d7c11bcc1c44cc5f348b
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-07-26 18:10:59 +0200

    Logging In users with API

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { showAlert } from './alert';
+
+export const login = async (email, password) => {
+  try {
+    const result = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    // we are really sure that our API call was successful
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
